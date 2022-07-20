b263356e82daa077cc15738b1e226c78a4938b6a
Autumn Meadow <blazeykirin@gmail.com>
2022-07-19 21:03:08 -0700

    we github zen lol

---
+++ src/bin/pulsar-migrator-issue-bot.rs
@@ -0,0 +1,27 @@
+use hyper::body::Body;
+use hyper::body::HttpBody as _;
+use hyper::{ Method, Request };
+use pulsar_migrator_issue_bot::http::get_http_client;
+use pulsar_migrator_issue_bot::Result;
+
+#[tokio::main]
+async fn main() -> Result {
+	let client = get_http_client();
+
+	let request = Request::builder()
+		.uri("https://api.github.com/zen")
+		.header("user-agent", "pulsar-migrator-issue-bot (https://github.com/autumnblazey/pulsar-migrator-issue-bot)")
+		.method(Method::GET)
+		.body(Body::empty())?;
+
+	let mut zen = client.request(request).await?;
+	let body = zen.body_mut();
+	let mut zen = String::new();
+
+	while let Some(chunk) = body.data().await {
+		zen.push_str(&String::from_utf8(chunk?.to_vec())?);
+	}
+	println!("{}", zen);
+
+	Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
+pub mod http;
+
+pub type Result<T = ()> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
